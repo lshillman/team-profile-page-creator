6ca7a5eeca09aec8e208eb11ecd971049f4c099f
lshillman <lshillman@users.noreply.github.com>
2022-07-20 21:30:23 -0700

    add all unit tests for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,21 @@
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee'); // make sure we can access the Employee class
 
-const borg = new Employee("Bob", 423, "bob@bob.com")
+const borg = new Employee("Hugh", 35, "hugh@collective.com"); // create a new borg for our tests below
 
 describe("Employee class", () => {
     it("has a name which is accessible via the getName function", () => {
-        expect(borg.getName()).toBe("Bob")
+        expect(borg.getName()).toBe("Hugh");
+    });
+
+    it("has an ID which is accessible via the getId function", () => {
+        expect(borg.getId()).toBe(35);
+    });
+
+    it("has an email which is accessible via the getEmail function", () => {
+        expect(borg.getEmail()).toBe("hugh@collective.com");
+    });
+
+    it("has a role of 'Employee', accessible via the getRole function", () => {
+        expect(borg.getRole()).toBe("Employee");
     });
 });
